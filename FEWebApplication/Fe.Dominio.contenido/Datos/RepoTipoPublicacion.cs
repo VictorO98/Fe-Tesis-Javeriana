6ada6f02989abdaf9df8ab244e9d922c08574d9a
Victor Ospina <ospiman98@gmail.com>
2021-05-28 02:43:40 -0500

    Métodos de validar tipode publicacion, validar demografia

---
+++ FEWebApplication/Fe.Dominio.contenido/Datos/RepoTipoPublicacion.cs
@@ -0,0 +1,18 @@
+﻿using Fe.Servidor.Middleware.Modelo.Contexto;
+using Fe.Servidor.Middleware.Modelo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fe.Dominio.contenido.Datos
+{
+    public class RepoTipoPublicacion
+    {
+        internal TipoPublicacionPc GetTipoPublicacionPorID(int idPublicacion)
+        {
+            using FeContext context = new FeContext();
+            return (TipoPublicacionPc)context.TipoPublicacionPcs.Where(p => p.Id == idPublicacion);
+        }
+    }
+}
