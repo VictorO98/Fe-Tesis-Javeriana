9b7025b736254368d1f2db64185dce929aa3150c
Antonio Yu <antonio.yu1998@gmail.com>
2021-05-27 20:14:55 -0500

    Endpoint de Get Categoria y Get Categoria por ID

---
+++ FEWebApplication/FEWebApplication/Controladores/Contenido/COApiController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FEWebApplication.Controladores.Contenido
+{
+    [ApiController]
+    [Produces("application/json")]
+    public abstract class COApiController : ControllerBase
+    {
+    }
+}
---
+++ FEWebApplication/FEWebApplication/Controladores/Contenido/COController.cs
@@ -0,0 +1,37 @@
+using Fe.Dominio.contenido.Datos;
+using Fe.Dominio.contenido.Negocio;
+using Fe.Dominio.contenido;
+using Fe.Core.Global.Constantes;
+using Fe.Core.Global.Errores;
+using Fe.Core.General.Datos;
+using Fe.Servidor.Middleware.Modelo.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FEWebApplication.Controladores.Contenido
+{
+    [Route("contenido/[controller]")]
+    public class COController : COApiController
+    {   
+        private readonly COFachada _coFachada;
+
+        public COController(COBiz cObiz, COFachada cOFachada)
+        {
+            _coFachada = cOFachada;
+        }
+
+        [Route("GetCategorias")]
+        [HttpGet]
+        public List<CategoriaPc> GetCategorias()
+        {
+            return _coFachada.GetCategorias();
+        }
+
+        [Route("GetCategoriaPorIdCategoria")]
+        [HttpGet]
+        public CategoriaPc GetCategoriaPorIdCategoria(int idCategoria)
+        {
+            return _coFachada.GetCategoriaPorIdCategoria();
+        }
+    }
+}
