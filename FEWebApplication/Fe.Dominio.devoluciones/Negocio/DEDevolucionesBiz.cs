9e835af2dc9081edcd63c6125314948851422c94
Victor Ospina <ospiman98@gmail.com>
2021-05-27 22:30:05 -0500

    Integracion

---
+++ FEWebApplication/Fe.Dominio.devoluciones/Negocio/DEDevolucionesBiz.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fe.Dominio.devoluciones.Negocio
+{
+    public class DEDevolucionesBiz
+    {
+    }
+}
---
+++ FEWebApplication/Fe.Dominio.facturas/Negocio/FAFacturaBiz.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fe.Dominio.facturas.Negocio
+{
+    class FAFacturaBiz
+    {
+    }
+}
