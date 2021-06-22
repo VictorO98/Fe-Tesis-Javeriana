bdb9c1aa993fecfb4f7ba3202cea82daccf6815e
Victor Ospina <ospiman98@gmail.com>
2021-06-21 20:31:22 -0500

    Archivo estado pedidos

---
+++ FEWebApplication/Fe.Core.Global/Constantes/COEstadoPedido.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fe.Core.Global.Constantes
+{
+    public class COEstadoPedido
+    {
+        public const string GENERADO = "GEN";
+        public const string CANCELADO = "CAN";
+        public const string RECHAZADO = "REC";
+    }
+}
